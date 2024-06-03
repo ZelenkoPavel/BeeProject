6e1b2dc94fc4db146ec56a91ab8982fa1d4bd7ec
tokio220506.pavel@gmail.com <tokio220506.pavel@gmail.com>
2024-06-03 17:13:31 +0300

    class WorkerBee

--- BeeProject/WorkerBee.cpp
+++ BeeProject/WorkerBee.cpp
@@ -1,1 +1,32 @@
 #include "WorkerBee.h"
+
+string WorkerBee::getTypeOfCollect() {
+	return type_of_collect;
+}
+void WorkerBee::setTypeOfCollect(string type_of_collect) {
+	this->type_of_collect = type_of_collect;
+}
+
+double WorkerBee::getDistance() {
+	return distance;
+}
+void WorkerBee::setDistance(double distance) {
+	if (distance >= 0) {
+		this->distance = distance;
+	}
+}
+
+double WorkerBee::the_amount_of_honey_consumed() {
+	return honey_consumption + honey_consumption * distance / 100;
+}
+string WorkerBee::getInfo() {
+	string msg = "Worker Bee:\n"
+		+ Bee::getInfo()
+		+ to_string(the_amount_of_honey_consumed());
+	msg += ";\ndistance - "
+		+ to_string(distance);
+	msg += ";\nType of collect - "
+		+ type_of_collect;
+
+	return msg;
+}
--- BeeProject/WorkerBee.h
+++ BeeProject/WorkerBee.h
@@ -3,9 +3,23 @@
 class WorkerBee : public Bee
 {
 private:
-
+	string type_of_collect = "super mega hyper worker";
+	double distance; // in meters
 public:
+	WorkerBee() : Bee(), distance(1) {}
 
+	WorkerBee(int life_in_days) : Bee(life_in_days), distance(1) {}
+
+	WorkerBee(double distance) : Bee(), distance(distance) {}
+
+	WorkerBee(int life_in_days, double distance) 
+		: Bee(life_in_days), distance(distance) {}
+
+	string getTypeOfCollect();
+	void setTypeOfCollect(string type_of_collect);
+
+	double getDistance();
+	void setDistance(double distance);
 
 	double the_amount_of_honey_consumed() override;
 	string getInfo() override;
