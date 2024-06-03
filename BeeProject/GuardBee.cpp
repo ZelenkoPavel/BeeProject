3449bb44aa3e70d5102693c02e7bb48d00d11625
tokio220506.pavel@gmail.com <tokio220506.pavel@gmail.com>
2024-06-03 17:21:40 +0300

    class GuardBee

--- BeeProject/GuardBee.cpp
+++ BeeProject/GuardBee.cpp
@@ -1,1 +1,23 @@
 #include "GuardBee.h"
+
+int GuardBee::getNumberOfAttacks() {
+	return number_of_attacks;
+}
+void GuardBee::setNumberOfAttacks(int number_of_attacks) {
+	if (number_of_attacks >= 0) {
+		this->number_of_attacks = number_of_attacks;
+	}
+}
+
+double GuardBee::the_amount_of_honey_consumed() {
+	return honey_consumption + honey_consumption * number_of_attacks / 1000;
+}
+string GuardBee::getInfo() {
+	string msg = "Scout Bee:\n"
+		+ Bee::getInfo()
+		+ to_string(the_amount_of_honey_consumed());
+	msg += ";\nNumber of attacks - "
+		+ to_string(number_of_attacks);
+
+	return msg;
+}
--- BeeProject/GuardBee.h
+++ BeeProject/GuardBee.h
@@ -3,9 +3,20 @@
 class GuardBee : public Bee
 {
 private:
-
+	int number_of_attacks;
 public:
+	GuardBee() : Bee(), number_of_attacks(0) {}
 
+	GuardBee(int life_in_days) : Bee(life_in_days), number_of_attacks(0) {}
+
+	GuardBee(int number_of_attacks) : Bee(), number_of_attacks(number_of_attacks) {}
+
+	GuardBee(int life_in_days, int number_of_attacks)
+		: Bee(life_in_days), number_of_attacks(number_of_attacks) {}
+
+
+	int getNumberOfAttacks();
+	void setNumberOfAttacks(int number_of_attacks);
 
 	double the_amount_of_honey_consumed() override;
 	string getInfo() override;
