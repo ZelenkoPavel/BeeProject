98149ead4ec29c9bfaf80a08454723451b697e5e
tokio220506.pavel@gmail.com <tokio220506.pavel@gmail.com>
2024-06-03 17:15:44 +0300

    class ScoutBee

--- BeeProject/ScoutBee.cpp
+++ BeeProject/ScoutBee.cpp
@@ -1,1 +1,24 @@
 #include "ScoutBee.h"
+
+
+double ScoutBee::getDistance() {
+	return distance;
+}
+void ScoutBee::setDistance(double distance) {
+	if (distance >= 0) {
+		this->distance = distance;
+	}
+}
+
+double ScoutBee::the_amount_of_honey_consumed() {
+	return honey_consumption + honey_consumption * distance / 100;
+}
+string ScoutBee::getInfo() {
+	string msg = "Scout Bee:\n"
+		+ Bee::getInfo()
+		+ to_string(the_amount_of_honey_consumed());
+	msg += ";\ndistance - "
+		+ to_string(distance);
+
+	return msg;
+}
--- BeeProject/ScoutBee.h
+++ BeeProject/ScoutBee.h
@@ -3,9 +3,20 @@
 class ScoutBee : public Bee
 {
 private:
-
+	double distance; // in meters
 public:
+	ScoutBee() : Bee(), distance(1) {}
 
+	ScoutBee(int life_in_days) : Bee(life_in_days), distance(1) {}
+
+	ScoutBee(double distance) : Bee(), distance(distance) {}
+
+	ScoutBee(int life_in_days, double distance)
+		: Bee(life_in_days), distance(distance) {}
+
+
+	double getDistance();
+	void setDistance(double distance);
 
 	double the_amount_of_honey_consumed() override;
 	string getInfo() override;
