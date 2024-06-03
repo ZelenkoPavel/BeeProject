1b46e08978fd02c02e6697a23e367b47aec00917
tokio220506.pavel@gmail.com <tokio220506.pavel@gmail.com>
2024-06-03 18:31:34 +0300

    class BuilderBee

--- BeeProject/BuilderBee.cpp
+++ BeeProject/BuilderBee.cpp
@@ -1,1 +1,24 @@
 #include "BuilderBee.h"
+
+double BuilderBee::getTheAmountOfWork() {
+	return the_amount_of_work;
+}
+void BuilderBee::setTheAmountOfWork(double the_amount_of_work) {
+	if (the_amount_of_work >= 0) {
+		this->the_amount_of_work = the_amount_of_work;
+	}
+}
+
+double BuilderBee::the_amount_of_honey_consumed() {
+	return Bee::the_amount_of_honey_consumed() * the_amount_of_work
+		/ 1000 + Bee::the_amount_of_honey_consumed();
+}
+string BuilderBee::getInfo() {
+	string msg = "Scout Bee:\n"
+		+ Bee::getInfo()
+		+ to_string(the_amount_of_honey_consumed());
+	msg += ";\nThe amount of work - "
+		+ to_string(the_amount_of_work);
+
+	return msg;
+}
--- BeeProject/BuilderBee.h
+++ BeeProject/BuilderBee.h
@@ -3,9 +3,21 @@
 class BuilderBee : public Bee
 {
 private:
-
+	double the_amount_of_work;
 public:
+	BuilderBee() : Bee(), the_amount_of_work(0) {}
 
+	BuilderBee(int life_in_days) : Bee(life_in_days), the_amount_of_work(0) {}
+
+	BuilderBee(double the_amount_of_work) 
+		: Bee(), the_amount_of_work(the_amount_of_work) {}
+
+	BuilderBee(int life_in_days, double the_amount_oà_work)
+		: Bee(life_in_days), the_amount_of_work(the_amount_of_work) {}
+
+
+	double getTheAmountOfWork();
+	void setTheAmountOfWork(double the_amount_of_work);
 
 	double the_amount_of_honey_consumed() override;
 	string getInfo() override;
